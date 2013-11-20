ca5f7c06677fb1c5ffa6af18a8abb76c311a827d
Jeff Kiske <jkiske09@gmail.com>
2013-11-19 20:34:33 -0800

    Prettified the file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var _und = require("underscore");
-var io = require("socket.io")
+var io = require("socket.io");
 var http = require('http');
 var fs = require('fs');
 var $ = require('jquery');
@@ -18,8 +18,8 @@ server.listen(port);
 
 app.use(express.static(__dirname + '/assets'));
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/assets/index.html');
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/assets/index.html');
 });
 
 
@@ -32,147 +32,150 @@ var tables = {};
 
 var waiting_room = 'waiting room';
 
-socket.sockets.on('connection', function (client) {
+socket.sockets.on('connection', function(client) {
     //When someone connects put them in the waiting room
     client.join(waiting_room);
 
     //Let the new client know which tables are available
     _und.each(_und.values(tables),
-	      function(table) {
-		  client.emit("addTableRow", JSON.stringify(table.safe()));
-	      }
-	     );
+        function(table) {
+            client.emit("addTableRow", JSON.stringify(table.safe()));
+        }
+    );
 
     function joinTable(table_id, playerName) {
-	var all_names = _und.pluck(_und.values(players), "name");
-	if (_und.contains(all_names, playerName)) {
-	    //Don't allow duplicate players
-	    client.emit("duplicateName", playerName);
-	    return false;
-	} else {
-	    if (table_id in tables) {
-		var player = makePlayer(client, playerName);
-		var table = tables[table_id];
+        var all_names = _und.pluck(_und.values(players), "name");
+        if (_und.contains(all_names, playerName)) {
+            //Don't allow duplicate players
+            client.emit("duplicateName", playerName);
+            return false;
+        } else {
+            if (table_id in tables) {
+                var player = makePlayer(client, playerName);
+                var table = tables[table_id];
 
-		player.table = table.id;
-		table.players[playerName] = player;
+                player.table = table.id;
+                table.players[playerName] = player;
 
-		player.position = table.firstOpenPosition();
-		table.positions[player.position] = player.name;
+                player.position = table.firstOpenPosition();
+                table.positions[player.position] = player.name;
 
-		client.join(table.id);
-		client.leave(waiting_room);
+                client.join(table.id);
+                client.leave(waiting_room);
 
-		//Tell this client to join the table
-		client.emit("joinTable", JSON.stringify(table.safe()));
+                //Tell this client to join the table
+                client.emit("joinTable", JSON.stringify(table.safe()));
 
-		updatePlayerPositions(table);
-		return true;
-	    }
-	}
-	return false;
+                updatePlayerPositions(table);
+                return true;
+            }
+        }
+        return false;
     }
 
     function updatePlayerPositions(table) {
-	// Put all of the other players into a map - position:{name: ?, score: ?}
-	var table_players = _und.values(table.players);
-	var other_pos = _und.filterAndIndexBy(table_players, "position", ["name", "score"]);
+        // Put all of the other players into a map - position:{name: ?, score: ?}
+        var table_players = _und.values(table.players);
+        var other_pos = _und.filterAndIndexBy(table_players, "position", ["name", "score"]);
 
-	//Tell all the clients at the table that there is a new player
-	var clients = socket.sockets.clients(table.id);
-	_und.each(clients, function(c) {
-	    //Send the client his position
-	    var client_pos = players[c.id].position;
-	    c.emit("updatePositions", JSON.stringify(client_pos),
-		                      JSON.stringify(other_pos));
-	});
+        //Tell all the clients at the table that there is a new player
+        var clients = socket.sockets.clients(table.id);
+        _und.each(clients, function(c) {
+            //Send the client his position
+            var client_pos = players[c.id].position;
+            c.emit("updatePositions", JSON.stringify(client_pos),
+                JSON.stringify(other_pos));
+        });
 
-	//Tell all the clients in the waiting room that there is an update
-	socket.sockets.in(waiting_room).emit("updateTableRow",
-					     JSON.stringify(table.safe()));
+        //Tell all the clients in the waiting room that there is an update
+        socket.sockets. in (waiting_room).emit("updateTableRow",
+            JSON.stringify(table.safe()));
     }
 
     client.on('joinTable', joinTable);
 
     client.on('newTable', function(playerName) {
-	var table = makeTable();
-	//Tell all the clients in the waiting room that there is an update
-	socket.sockets.in(waiting_room).emit("addTableRow",
-					     JSON.stringify(table.safe()));
+        var table = makeTable();
+        //Tell all the clients in the waiting room that there is an update
+        socket.sockets. in (waiting_room).emit("addTableRow",
+            JSON.stringify(table.safe()));
 
-	//Check to see if we successfully joined
-	var did_join = joinTable(table.id, playerName);
-	if (!did_join) {
-	    socket.sockets.in(waiting_room).emit("removeTableRow", table.id);
-	    //Delete the table if there was an error
-	    delete tables[table.id];
-	}
+        //Check to see if we successfully joined
+        var did_join = joinTable(table.id, playerName);
+        if (!did_join) {
+            socket.sockets. in (waiting_room).emit("removeTableRow", table.id);
+            //Delete the table if there was an error
+            delete tables[table.id];
+        }
     });
 
     // Individual table logic
-    client.on('dealCards', function(){
-	var player = players[client.id];
-	var table = tables[player.table];
-	var deck = table.deck;
+    client.on('dealCards', function() {
+        var player = players[client.id];
+        var table = tables[player.table];
+        var deck = table.deck;
 
-	if (_und.size(player.cards) < 13) {
-	    var cards = deck.draw(13, "", true);
-//	    cards = _und.sortBy(cards, function(card) {
-//		return deck.sortValue(card);
-//	    });
+        if (_und.size(player.cards) < 13) {
+            var cards = deck.draw(13, "", true);
+            //		cards = _und.sortBy(cards, function(card) {
+            //		return deck.sortValue(card);
+            //		});
 
-	    player.cards = cards;
-	    console.log("Added cards to player " + player.name);
-	    client.emit('showCards', JSON.stringify(cards));
-	} else {
-	    console.log("Player " + player.name + " already has 13 cards");
-	}
+            player.cards = cards;
+            console.log("Added cards to player " + player.name);
+            client.emit('showCards', JSON.stringify(cards));
+        } else {
+            console.log("Player " + player.name + " already has 13 cards");
+        }
     });
 
     client.on('submitCard', function(card) {
-	var card_suit = card.slice(0,1);
-	var card_rank = card.slice(1)-0; //Convert to an int?
-	var player = players[client.id];
-	if (player !== undefined) {
-	    var player_card = _und.where(player.cards, {suit:card_suit, rank:card_rank});
-	    //remove the card from the players deck
-	    //TODO: check if the card is in the players hand
-	    player.cards = _und.difference(player.cards, player_card);
-	    client.emit('showCards', JSON.stringify(player.cards));
+        var card_suit = card.slice(0, 1);
+        var card_rank = card.slice(1) - 0; //Convert to an int?
+        var player = players[client.id];
+        if (player !== undefined) {
+            var player_card = _und.where(player.cards, {
+                suit: card_suit,
+                rank: card_rank
+            });
+            //remove the card from the players deck
+            //TODO: check if the card is in the players hand
+            player.cards = _und.difference(player.cards, player_card);
+            client.emit('showCards', JSON.stringify(player.cards));
 
-	    //Send to all players except the emitting socket
-	    client.broadcast.to(player.table).emit("opponentPlayedCard", 
-						       JSON.stringify(player.name),
-						       JSON.stringify(card));
-	}
+            //Send to all players except the emitting socket
+            client.broadcast.to(player.table).emit("opponentPlayedCard",
+                JSON.stringify(player.name),
+                JSON.stringify(card));
+        }
     });
 
     //Disconnect
-    client.on('disconnect', function(){
-	if (client.id in players) {
-	    var player = players[client.id];
-	    delete players[client.id];
+    client.on('disconnect', function() {
+        if (client.id in players) {
+            var player = players[client.id];
+            delete players[client.id];
 
-	    var table = tables[player.table];
-	    if (table !== undefined) {
-		table.positions[player.position] = null;
+            var table = tables[player.table];
+            if (table !== undefined) {
+                table.positions[player.position] = null;
 
-		delete table.players[player.name];
-		client.leave(table.id);
+                delete table.players[player.name];
+                client.leave(table.id);
 
-		// If that was the last player in the room, delete the room
-		if (_und.size(table.players) == 0) {
-		    delete tables[table.id];
-		    socket.sockets.in(waiting_room).emit("removeTableRow", table.id);
-		} else {
-		    //Otherwise, remove the username from the row
-		    socket.sockets.in(waiting_room).emit("updateTableRow",
-							 JSON.stringify(table.safe()));
-		    //And let everone in the room know that person left
-		    updatePlayerPositions(table);
-		}
-	    }
-	}
+                // If that was the last player in the room, delete the room
+                if (_und.size(table.players) === 0) {
+                    delete tables[table.id];
+                    socket.sockets. in (waiting_room).emit("removeTableRow", table.id);
+                } else {
+                    //Otherwise, remove the username from the row
+                    socket.sockets. in (waiting_room).emit("updateTableRow",
+                        JSON.stringify(table.safe()));
+                    //And let everone in the room know that person left
+                    updatePlayerPositions(table);
+                }
+            }
+        }
     });
 
 
@@ -194,21 +197,22 @@ socket.sockets.on('connection', function (client) {
  */
 _und.mixin({
     filterAndIndexBy: function(obj, index, filter) {
-	// Put all of the other players into a map - pos:{name: ?, score: ?}
-	var vals = _und.values(obj);
-	var newObj = {};
-	// Iterate through the table's players and put them in the map
-	_und.each(vals, function(val) {
-	    var args = _und.union(val, filter);
-	    newObj[val[index]] = _und.pick.apply(this, args);
-	});
-	return newObj;
+        // Put all of the other players into a map - pos:{name: ?, score: ?}
+        var vals = _und.values(obj);
+        var newObj = {};
+        // Iterate through the table's players and put them in the map
+        _und.each(vals, function(val) {
+            var args = _und.union(val, filter);
+            newObj[val[index]] = _und.pick.apply(this, args);
+        });
+        return newObj;
     }
 });
 
 /**
  * Simple method to create and associate a player with an id
  */
+
 function makePlayer(client, name) {
     var player = new _player.Player(name, client.id);
     // Add the player to the list of global players
@@ -219,6 +223,7 @@ function makePlayer(client, name) {
 /**
  * Simple method to create and associate a table with an id
  */
+
 function makeTable() {
     var table = new _table.Table();
     // Add the table to the list of global tables
