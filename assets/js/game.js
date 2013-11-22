2c13e020dc01686c3643e6fbb0d5106c396aa3aa
Jeff Kiske <jkiske09@gmail.com>
2013-11-21 17:15:02 -0800

    Pretty formatting for game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var socket = io.connect("http://localhost:8888");
 
     var state = 'waiting'; // waiting, trading, playing
@@ -14,15 +14,16 @@ $(document).ready(function(){
     $('#game').hide();
 
     // -------------------------------- Name logic ----------------------------- //
+
     function validateName() {
-	var namedom = $("#playername");
-	var buttons = $(".joinbtn,#newtable");
-	if (namedom.val().length > 0) {
-	    buttons.removeClass("disabled");
-	} else {
-	    buttons.addClass("disabled");
-	}
-	return namedom.val().length > 0;
+        var namedom = $("#playername");
+        var buttons = $(".joinbtn,#newtable");
+        if (namedom.val().length > 0) {
+            buttons.removeClass("disabled");
+        } else {
+            buttons.addClass("disabled");
+        }
+        return namedom.val().length > 0;
     }
     // When we start, check see if we should disable links
     validateName();
@@ -32,220 +33,229 @@ $(document).ready(function(){
 
 
     // -------------------------------- Joining Tables ----------------------------- //
-    function rowHtml(table) {
-	var player_names = table['players'].join(", ");
-	var round = table['round'];
-	var t_id = table['id'];
 
-	return '<tr>' +
-	    '<td>' + player_names + '</td>' +
-	    '<td>' + round + '</td>' +
-	    '<td><div  id = "' + t_id + '" class=' +
-	    '"joinbtn text-center btn btn-sm btn-primary">' +
-	    '<span class="glyphicon glyphicon-chevron-right"></span>' +
-	    '</div></td>' +
-	    '</tr>';
+    function rowHtml(table) {
+        var player_names = table['players'].join(", ");
+        var round = table['round'];
+        var t_id = table['id'];
+
+        return '<tr>' +
+            '<td>' + player_names + '</td>' +
+            '<td>' + round + '</td>' +
+            '<td><div  id = "' + t_id + '" class=' +
+            '"joinbtn text-center btn btn-sm btn-primary">' +
+            '<span class="glyphicon glyphicon-chevron-right"></span>' +
+            '</div></td>' +
+            '</tr>';
 
     }
     $("#newtable").click(function() {
-	if (validateName() == true) {
-	    //Prevent being able to double-click new game
-	    var buttons = $(".joinbtn,#newtable");
-	    buttons.addClass("disabled");
-	    socket.emit("newTable", $("#playername").val());
-	}
+        if (validateName() == true) {
+            //Prevent being able to double-click new game
+            var buttons = $(".joinbtn,#newtable");
+            buttons.addClass("disabled");
+            socket.emit("newTable", $("#playername").val());
+        }
     });
 
     //Add a new table to all the users that are still looking for one
     socket.on("addTableRow", function(table) {
-	var table = $.parseJSON(table)
-	$('#tabletable-id tbody').append(rowHtml(table));
+        var table = $.parseJSON(table)
+        $('#tabletable-id tbody').append(rowHtml(table));
 
-	//When we add a new table, check to see if we should make links inactive
-	validateName();
+        //When we add a new table, check to see if we should make links inactive
+        validateName();
 
-	// If we click the button, join that table
-	$("#"+table.id).click(function() {
-	    if (validateName() == true) {
-		//Prevent being able to double-click new game
-		var buttons = $(".joinbtn,#newtable");
-		buttons.addClass("disabled");
-		socket.emit("joinTable", table.id, $("#playername").val());
-	    }
-	});
+        // If we click the button, join that table
+        $("#" + table.id).click(function() {
+            if (validateName() == true) {
+                //Prevent being able to double-click new game
+                var buttons = $(".joinbtn,#newtable");
+                buttons.addClass("disabled");
+                socket.emit("joinTable", table.id, $("#playername").val());
+            }
+        });
     });
 
     socket.on("updateTableRow", function(table) {
-	var table = $.parseJSON(table);
-	var row = $("#"+table.id).closest("tr");
-	row.replaceWith(rowHtml(table));
+        var table = $.parseJSON(table);
+        var row = $("#" + table.id).closest("tr");
+        row.replaceWith(rowHtml(table));
 
-	// When we re-add the row we have to reattach the handler
-	$("#"+table.id).click(function() {
-	    if (validateName() == true) {
-		var buttons = $(".joinbtn,#newtable");
-		buttons.addClass("disabled");
-		socket.emit("joinTable", table.id, $("#playername").val());
-	    }
-	});
+        // When we re-add the row we have to reattach the handler
+        $("#" + table.id).click(function() {
+            if (validateName() == true) {
+                var buttons = $(".joinbtn,#newtable");
+                buttons.addClass("disabled");
+                socket.emit("joinTable", table.id, $("#playername").val());
+            }
+        });
     });
 
     socket.on("removeTableRow", function(table_id) {
-	$("#"+table_id).closest("tr").remove();
+        $("#" + table_id).closest("tr").remove();
     });
 
     var alertDialog = $("#name-taken");
     alertDialog.parent().hide();
     $('.alert .close').on("click", function() {
-	$(this).parent().hide();
+        $(this).parent().hide();
     });
     socket.on("duplicateName", function(name) {
-	//Clear the text
-	alertDialog.text("");
-	alertDialog.append("<strong>" + name +
-			   "</strong> is already playing. Please choose another name");
-	alertDialog.parent().show();
+        //Clear the text
+        alertDialog.text("");
+        alertDialog.append("<strong>" + name +
+            "</strong> is already playing. Please choose another name");
+        alertDialog.parent().show();
     });
 
     // -------------------------------- Switching Views ----------------------------- //
     //Switch views to the table view
     socket.on("joinTable", function(table) {
-	$('#tableslist').hide();
-	$('#game').show();
+        $('#tableslist').hide();
+        $('#game').show();
 
-	//var table = $.parseJSON(table);
-	//var players = _.values(table.players);
+        //var table = $.parseJSON(table);
+        //var players = _.values(table.players);
     });
 
 
     // -------------------------------- Game ----------------------------- //
 
     $("#play").click(function() {
-	$("#play").hide();
-	$("#played-cards").removeClass("hidden");
-	socket.emit("dealCards");
+        $("#play").hide();
+        $("#played-cards").removeClass("hidden");
+        socket.emit("dealCards");
 
-	state = 'trading';
-	$('#pass-btn').removeClass('hidden');
+        state = 'trading';
+        $('#pass-btn').removeClass('hidden');
     });
 
     socket.on("updatePositions", function(your_pos, all_pos) {
-	var your_pos = $.parseJSON(your_pos);
+        var your_pos = $.parseJSON(your_pos);
 
-	//A map from position to {name: ?, score: ?}
-	var all_pos = $.parseJSON(all_pos);
+        //A map from position to {name: ?, score: ?}
+        var all_pos = $.parseJSON(all_pos);
 
-	var dir_map = ["bottom", "right", "top", "left"];
-	var pos_map =["N", "W", "S", "E"];
-	//Rotate the table around based on our position
-	pos_map = _.rotate(pos_map, _.indexOf(pos_map, your_pos));
+        var dir_map = ["bottom", "right", "top", "left"];
+        var pos_map = ["N", "W", "S", "E"];
+        //Rotate the table around based on our position
+        pos_map = _.rotate(pos_map, _.indexOf(pos_map, your_pos));
 
-	var pos_dir_map =  _.object(pos_map, dir_map);
-	//Add players that exist
-	_.each(all_pos, function (player, pos) {
-	    var rel_dir = pos_dir_map[pos];
-	    var name = (player.name == null ? "Open" : player.name);
-	    var score = (player.score == undefined ? "0" : player.score);
-	    console.log(rel_dir + "is player: " + name + ": " + score);
-	    $("#"+rel_dir+"name").text(name + ": "+ score);
-	});
-	//Set ever other position to empty
-	_.each(pos_dir_map, function(rel_dir, pos) {
-	    if (_.contains(_.keys(all_pos), pos) == false) {
-		$("#"+rel_dir+"name").text("Open");
-	    }
-	});
+        var pos_dir_map = _.object(pos_map, dir_map);
+        //Add players that exist
+        _.each(all_pos, function(player, pos) {
+            var rel_dir = pos_dir_map[pos];
+            var name = (player.name == null ? "Open" : player.name);
+            var score = (player.score == undefined ? "0" : player.score);
+            console.log(rel_dir + "is player: " + name + ": " + score);
+            $("#" + rel_dir + "name").text(name + ": " + score);
+        });
+        //Set ever other position to empty
+        _.each(pos_dir_map, function(rel_dir, pos) {
+            if (_.contains(_.keys(all_pos), pos) == false) {
+                $("#" + rel_dir + "name").text("Open");
+            }
+        });
 
     });
 
     //Deal the cards to each player
     socket.on("showCards", showCards);
 
-    function showCards(cards){
-	var _cards = $.parseJSON(cards);
+    function showCards(cards) {
+        var _cards = $.parseJSON(cards);
 
-	_cards = _.sortBy(_cards, function(card) {
+        _cards = _.sortBy(_cards, function(card) {
             return sortValue(card);
         });
 
-	var bottomCards = $('#bottomcards');
+        var bottomCards = $('#bottomcards');
 
-	//Delete the children and replace them
-	bottomCards.children().remove();
-	$.each(_cards, function(key, value) {
-	    bottomCards.append('<li>' +
-			       createCard(value['suit'], value['rank'], 'a') +
-			       '</li>'
-			      );
-	});
+        //Delete the children and replace them
+        bottomCards.children().remove();
+        $.each(_cards, function(key, value) {
+            bottomCards.append('<li>' +
+                createCard(value['suit'], value['rank'], 'a') +
+                '</li>'
+            );
+        });
         var measureCard = $('#bottomcards li');
-	// Magic numbers to center stack
-	bottomCards.css("margin-left", measureCard.length * - measureCard.width() / 2 - 56);
+        // Magic numbers to center stack
+        bottomCards.css("margin-left", measureCard.length * -measureCard.width() / 2 - 56);
 
-	$("a.card").click(function() {
-	    if (state == 'trading') {
-		var openSlots = $('.empty-card-slot');
-		if (openSlots.length > 0) {
-		    var slot = $(openSlots[0]);
-		    exchangeCard(slot, $(this), false);
-		    slot.removeClass('empty-card-slot');
-		    slot.removeClass('hide-card');
+        $("a.card").click(function() {
+            if (state == 'trading') {
+                var openSlots = $('.empty-card-slot');
+                if (openSlots.length > 0) {
+                    var slot = $(openSlots[0]);
+                    exchangeCard(slot, $(this), false);
+                    slot.removeClass('empty-card-slot');
+                    slot.removeClass('hide-card');
 
-		    slot.addClass('filled-card-slot');
+                    slot.addClass('filled-card-slot');
 
-		    // Add a click handler to return card back to deck
-		    slot.click(function() {
-			if ($(this).hasClass('hidden'))
-			    return true;
-		    });
+                    // Add a click handler to return card back to deck
+                    slot.click(function() {
+                        if ($(this).hasClass('hidden'))
+                            return true;
+                    });
 
-		    if (openSlots.length == 1) {
-			$('#pass-btn').removeClass('disabled');
-		    }
-		}
-	    }
-	    else if (state == 'playing') {
-		exchangeCard(bottomCard, $(this), true);
-		bottomCard.removeClass('hide-card');
-	    }
+                    if (openSlots.length == 1) {
+                        $('#pass-btn').removeClass('disabled');
+                    }
+                }
+            } else if (state == 'playing') {
+                exchangeCard(bottomCard, $(this), true);
+                bottomCard.removeClass('hide-card');
+            }
 
-	    var measureCard = $('#bottomcards li');
-	    // Magic numbers to center stack
-	    bottomCards.css("margin-left", measureCard.length * - measureCard.width() / 2 - 56);
+            var measureCard = $('#bottomcards li');
+            // Magic numbers to center stack
+            bottomCards.css("margin-left", measureCard.length * -measureCard.width() / 2 - 56);
 
-	});
+        });
     }
 
     socket.on("opponentPlayedCard", function(name, card) {
-	var card = $.parseJSON(card);
-	var name = $.parseJSON(name);
-	console.log("Player " +name + "played card " + card);
+        var card = $.parseJSON(card);
+        var name = $.parseJSON(name);
+        console.log("Player " + name + "played card " + card);
     });
 
     function exchangeCard(card, deckCard, shouldEmit) {
-	//Get the rank/suit information
-	var id = deckCard.attr('id');
-	var suit = id.slice(0,1);
-	//Slice only becasue card can be A10
-	var rank = id.slice(1);
-	if (rank in inv_rankmap) {
-	    rank = inv_rankmap[rank];
-	}
+        //Get the rank/suit information
+        var id = deckCard.attr('id');
+        var suit = id.slice(0, 1);
+        //Slice only becasue card can be A10
+        var rank = id.slice(1);
+        if (rank in inv_rankmap) {
+            rank = inv_rankmap[rank];
+        }
 
-	//Replace the middle card with the deck card
-	card.find('.card').replaceWith(createCard(suit, rank, 'div'));
+        //Replace the middle card with the deck card
+        card.find('.card').replaceWith(createCard(suit, rank, 'div'));
 
-	//Remove the deck card
-	deckCard.closest('li').remove();
+        //Remove the deck card
+        deckCard.closest('li').remove();
 
-	if (shouldEmit)
-	    socket.emit('submitCard', suit+rank);
+        if (shouldEmit)
+            socket.emit('submitCard', suit + rank);
     }
 });
 
-var suitmap = {"H":"hearts" , "C":"clubs",
-	       "S":"spades", "D":"diams"};
-var rankmap = {1:"a", 11: "j", 12:"q", 13:"k"};
+var suitmap = {
+    "H": "hearts",
+    "C": "clubs",
+    "S": "spades",
+    "D": "diams"
+};
+var rankmap = {
+    1: "a",
+    11: "j",
+    12: "q",
+    13: "k"
+};
 var inv_rankmap = _.invert(rankmap);
 
 function createCard(suit, rank, tag) {
@@ -253,40 +263,45 @@ function createCard(suit, rank, tag) {
     var includeSuit = true;
 
     if (rank in rankmap) {
-	var rank = rankmap[rank];
-	if (rank == "a")
-	    includeSuit = false;
+        var rank = rankmap[rank];
+        if (rank == "a")
+            includeSuit = false;
     } else {
-	var rank = rank.toString();
-	includeSuit = false;
+        var rank = rank.toString();
+        includeSuit = false;
     }
 
     return "<" + tag + " id=\"" + suit + rank + "\" " +
-	"class=\"card rank-"+ rank + " " + full_suit + "\">\n" +
-	"<span class=\"rank\">" + rank.toUpperCase() + "</span>" +
-	"<span class=\"suit\">" +
-	(includeSuit ? ("&" + full_suit + ";") : "") +
-	"</span>" +
-	"</" + tag + ">";
+        "class=\"card rank-" + rank + " " + full_suit + "\">\n" +
+        "<span class=\"rank\">" + rank.toUpperCase() + "</span>" +
+        "<span class=\"suit\">" +
+        (includeSuit ? ("&" + full_suit + ";") : "") +
+        "</span>" +
+        "</" + tag + ">";
 }
 
 function sortValue(card) {
-        var suitVals = {"C":0, "D":1, "S":2, "H":3};
-        //Give each suit a value for sorting
-        var suitVal = suitVals[card["suit"]]*13;
-        //Aces are high
-        var rankVal = card["rank"] == 1 ? 13 : card["rank"] - 1;
-        return rankVal + suitVal;
+    var suitVals = {
+        "C": 0,
+        "D": 1,
+        "S": 2,
+        "H": 3
+    };
+    //Give each suit a value for sorting
+    var suitVal = suitVals[card["suit"]] * 13;
+    //Aces are high
+    var rankVal = card["rank"] == 1 ? 13 : card["rank"] - 1;
+    return rankVal + suitVal;
 }
 
 
 _.mixin({
     rotate: function(array, n, guard) {
-	var head, tail;
+        var head, tail;
         n = (n == null) || guard ? 1 : n;
         n = n % array.length;
         tail = array.slice(n);
-	head = array.slice(0, n);
-	return tail.concat(head);
+        head = array.slice(0, n);
+        return tail.concat(head);
     }
 });
